63e51f7d7f43b11e51decf1ae9b0aea7ac98a430
doorme777 <137806586+doorme777@users.noreply.github.com>
2024-04-16 10:14:29 -0600

    ➕ [ADD] Github pages dependecy
    Lista para hacer deploy con github pages.

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 8080,
   },
   preview: {
     port: 3000,
   },
-  plugins: [react()],
+  base: "https://doorme777.github.io/e-commerce/",
 })
